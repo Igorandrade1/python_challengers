201784859bac23d2948a08fae04a10c2c87a0a15
Igor Andrade <igor.andrade@novanet.local>
2023-02-09 16:49:36 -0300

    corrigidoo

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 n = int(input("digite o numero: "))
 
-# soma = 0
-# for i in range(0, n+1):
-#     if i % 2 != 0:
-#         soma += i
-#
-# print(f"a soma de todos os numeros impars até {n} é {soma}")
-
 result = (
     [i+i for i in range(0, n+1) if i % 2 != 0]
 )
-
 
 for x in range(0, 50):
     print(x)
